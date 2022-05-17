028961ac3c3f5a7f679a60b283cc5d78bbe80c29
Rohan9731 <rohanrrrs14@gmail.com>
2022-05-17 15:16:59 +0530

    p7 done

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,3 +1,4 @@
-# Strings
-
-text = "X-DSPAM-Confidence:    0.8475"
+print("welcome to shanks band name generator")
+a = input("What is the name of your city? ")
+b = input("What is the name of your pet? ")
+print("The name of your band could be : " + a + b)
