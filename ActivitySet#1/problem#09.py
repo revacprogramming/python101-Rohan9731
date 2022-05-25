34d30e791831bd203273c161c117cc3aaa6493a9
Rohan9731 <rohanrrrs14@gmail.com>
2022-05-25 23:52:46 +0530

    p9 done

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,22 @@
-# Lists
+# Use the file name mbox-short.txt as the file name
+count=0
+total=0
 
-filename = "dataset/romeo.txt"
+fname = input("Enter file name: ")
+try:
+	fh = open(fname)
+except:
+    print("entered file is invalid/not found:",fname)
+for line in fh:
+	if line.startswith("X-DSPAM-Confidence:"):
+		count=count+1
+		digit= line.find(":")
+		x= float(line[digit+1:])
+		total= total+x
+	else:
+		continue
+        
+value = total/count        
+        
+print("Average spam confidence:",value)
+
