3c094921e7c7afb68e8cb78d937cc22c14f8ecf0
Rohan9731 <rohanrrrs14@gmail.com>
2022-05-30 23:22:04 +0530

    p11 done

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,17 @@
-# Tuples
+count=0
+fname = input("Enter file name: ")
+try:
+    file=open(fname)
+except:
+    print("Entered file is not exist")
 
-filename = "dataset/mbox-short.txt"
+for line in file:
+    if line.startswith('From '):
+        word=line.rstrip().split()
+        count=count+1
+        print(word[1])
+    else:
+        continue
+   
+
+print("There were", count, "lines in the file with From as the first word")
