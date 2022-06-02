db28296aedc48ce4d451a43e9479f688fb679ba3
Rohan9731 <rohanrrrs14@gmail.com>
2022-06-03 00:22:32 +0530

    p13 done

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,2 +1,25 @@
 # Network Programming
 # https://www.py4e.com/lessons/network
+name = input("Enter file:")
+try:
+	handle = open(name)
+except:
+	print("invalid input")
+d={}
+
+for line in handle:
+	if line.startswith("From "):   
+		line=line.split()
+		line=line[5]
+		line=line[:2]
+		d[line]=d.get(line,0)+1
+	else:
+		continue        
+l=list()
+for key,value in d.items():
+	l.append((key,value))
+    
+l.sort()
+
+for key,value in l[:12]:
+	print(key,value)
