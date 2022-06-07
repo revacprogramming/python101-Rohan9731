23243124a98998278d60d2742f60eec8f5d72bf2
Rohan9731 <rohanrrrs14@gmail.com>
2022-06-07 23:57:12 +0530

    p1done

--- ActivitySet#2/problem#1.py
+++ ActivitySet#2/problem#1.py
@@ -3,13 +3,10 @@
 def add(x,y):
     z=  x+y
     return z
-
-
-
-a = float(input("Enter the first number: "))
-b = float(input("Enter the second number:"))
-c = add(a,b)
-    
-    
-    
-print ("sum of two given number is",c)
+def main():
+    a = float(input("Enter the first number: "))
+    b = float(input("Enter the second number:"))
+    c = add(a,b)
+    return c
+s=main()
+print("sum of two given number is",s)
