829bfb51d4059ac91fcc2417908335de39819036
Rohan9731 <rohanrrrs14@gmail.com>
2022-06-07 23:48:30 +0530

    working

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -2,3 +2,12 @@
 # https://www.py4e.com/lessons/servces
   
    #working
+import re
+fh = open("regex_sum_269097.txt")
+sum = 0
+for line in fh:
+   num = re.findall('[0-9]+',line)
+   length = len(num)
+   for i in range(length):
+      sum += int(num[i])
+print(sum)
