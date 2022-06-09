1a1e5d5415f39ab2dc068aff879d12902325f61b
Rohan9731 <rohanrrrs14@gmail.com>
2022-06-09 22:58:35 +0530

    working

--- ActivitySet#2/problem#2.py
+++ ActivitySet#2/problem#2.py
@@ -1,18 +1,18 @@
 
 def add(a, b):
-    pass  # ...
+    return a+b
 
 
 def output(a, b, sum):
-    pass  # ...
+    print("sum of given two numbers is",sum)
 
 
 def main():
-    a, b = input_two_numbers()
+    a = float(input("inputtwo_numbers:"))
+    b = float(input("enterthe secomd number:"))
     sum = add(a, b)
-
     output(a, b, sum)
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
