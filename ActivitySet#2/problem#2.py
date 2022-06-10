871963aa5a9e8e3ce89281ccbee190e51e8d26c2
Rohan9731 <rohanrrrs14@gmail.com>
2022-06-10 09:20:13 +0530

    p2 done

--- ActivitySet#2/problem#2.py
+++ ActivitySet#2/problem#2.py
@@ -4,16 +4,16 @@ def add(a, b):
 
 
 def output(a, b, sum):
-    print("sum of given two numbers is",sum)
+    print("sum of given two numbers ",a,"and" ,b,"is",sum)
 
 
 def main():
-    a = float(input("inputtwo_numbers:"))
-    b = float(input("enterthe secomd number:"))
+    a = float(input("input first_number:"))
+    b = float(input("enter the second number:"))
     sum = add(a, b)
     output(a, b, sum)
 
 
-# if __name__ == '__main__':
-#     main()
+if __name__ == '__main__':
+    main()
 
