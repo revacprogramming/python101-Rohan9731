ba9da1602e54dc313121232aa19042157a8aa61b
Rohan9731 <rohanrrrs14@gmail.com>
2022-07-17 23:47:23 +0530

    .

--- ActivitySet#1/problem#15.py
+++ ActivitySet#1/problem#15.py
@@ -1,4 +1,13 @@
-import re
-s = 'A message from csev@umich.edu to cwen@iupui.edu about meeting @2PM'
-lsted = re.findall('\S+@\S+', s)
-print(lsted)  
+from socket import *
+
+soc = socket(AF_INET,SOCK_STREAM)
+soc.connect(('data.pr4e.org',80))
+cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
+soc.send(cmd)
+
+while True:
+   data = soc.recv(512)
+   if(len(data)<1):
+      break
+   print(data.decode())
+soc.close()
